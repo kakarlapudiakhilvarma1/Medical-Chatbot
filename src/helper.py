44a97bf4ed221d3aebcba417d949480874fa42b2
Akhil Kakarlapudi <kakarlapudiakhilvarma1@gmail.com>
2024-11-21 13:06:39 +0530

    updated

---
+++ src/helper.py
@@ -0,0 +1,22 @@
+from langchain_community.document_loaders import PyPDFLoader, DirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_huggingface import HuggingFaceEmbeddings
+
+## Extract the data from the pdf
+def load_pdf_file(data):
+    loader = DirectoryLoader(data, glob="*.pdf", loader_cls=PyPDFLoader)
+
+    documents = loader.load()
+    return documents
+
+## Split the data into chuncks
+def text_split(extracted_data):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=20)
+    text_chunks = text_splitter.split_documents(extracted_data)
+    return text_chunks
+
+## Download the embedding from hugging face
+from langchain_huggingface import HuggingFaceEmbeddings
+def download_huggging_face_embeddings():
+    embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L12-v2")
+    return embeddings
